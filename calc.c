1b748931c3fef7cb442967c3082d8a4871591ffb
Fyodor Perevozchikov <31614726+IndianProgger@users.noreply.github.com>
2018-01-11 04:03:28 +0300

    Update calc.c

--- calc.c
+++ calc.c
@@ -18,7 +18,7 @@ int main() { /*initialising main function*/
 printf("To end work of this calculator enter '@'.\n");
     while (!feof(stdin)) {
         int c = getchar(); /*reading values from string*/
-     	int t,t2;
+     	int t,t2,t3;
         int x;
         switch (c) {
         	case '@': 
@@ -34,27 +34,25 @@ printf("To end work of this calculator enter '@'.\n");
         	printf("%d", t);}
             else{
              	t=-t;
-             	push(t);
-            }
+             	push(t); }
             t=0;
             break;
             case '*' : push(pop() * pop()); break;
             case '^' :
-            t=pop(); t2=pop();
-            if (t2>=0){
-            	for ( t2=t2; t2>1; t2=t2-1 )
-            	{
-            		t=t*t;
-            	} 
+            t2=pop(); t=pop(); t3=t;
+            if (t2<0) {
+            	printf("Sorry, this calculator can not work with negative degrees.\nFunction was ignored and value was returned to the massive. \n");
+            	push(t);
+            	break;
+        	}
+            if (t2==0) {
+            	t3=0;
             }
-            else{
-            	t=1/t;
-            	for ( t2=t2; t2<(-1); t2=t2+1 )
-            	{
-             		t=t/t;
-             	} 
+            while (t2>1){
+            	t2=t2-1;
+            	t3=t*t3; 
             }
-            push(t); t=0; t2=0;
+            push(t3); t=0; t2=0; t3=0;
             break;
             case '/' : t=pop(); push(pop()/ t); t=0; break;
             default:
